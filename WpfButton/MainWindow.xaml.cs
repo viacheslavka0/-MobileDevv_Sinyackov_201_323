234eca094596d0378f3ca36327a67194fe7bae90
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2022-10-20 12:40:56 +0300

    Кнопки в WPF

---
+++ WpfButton/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            meLogs.Text += "1";
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            meLogs.Text += "2";
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            var isChecked = (e.Source as ToggleButton)?.IsChecked; 
+
+            meLogs.Text += $"{isChecked}";
+        }
+    }
+}
