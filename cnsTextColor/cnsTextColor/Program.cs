0dc3654425f349eaae6dc041edefafef58ded9e3
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2022-11-05 18:04:47 +0300

    Вывод разных цветов в консоль

---
+++ cnsTextColor/cnsTextColor/Program.cs
@@ -0,0 +1,18 @@
+﻿var colors = Enum.GetValues<ConsoleColor>();
+
+
+
+foreach (var ColorText in colors)
+{
+    Console.ForegroundColor = ColorText;
+
+    foreach (var backColor in colors)
+    {
+        Console.BackgroundColor = backColor;
+
+        Console.Write("Hi");
+    }
+
+    Console.WriteLine();
+}
+Console.ResetColor();
