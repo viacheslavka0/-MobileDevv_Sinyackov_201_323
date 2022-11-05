f207743eb625ff4f5ecacd81365bd71e9b76f58d
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2022-11-05 17:48:29 +0300

    Прямоугольник

---
+++ cnsDrawRectangle/cnsDrawRectangle/Program.cs
@@ -0,0 +1,35 @@
+﻿//ДЗ: Нарисовать прямоугольник с параметрами "Ширина", "Высота", "Заливка(да/нет)",
+//"Символ дли рисовки", "Символ для заливки", повтор(как в примере с линией)
+using System;
+
+bool isContinue = true;
+bool isPouring = true;
+do
+{
+    
+    Console.WriteLine("Введите длину");
+    int.TryParse(Console.ReadLine(), out int x);
+
+    Console.WriteLine("Введите ширину");
+    int.TryParse(Console.ReadLine(), out int y);
+
+
+    for (int i = 0; i < y; i++)
+    {
+     
+      
+            if (i % (y - 1) == 0)
+            {
+                Console.WriteLine(new String('*', x));
+            }
+            else
+            {
+                Console.WriteLine('*' + new String(' ', x-2) + '*');
+            }
+        
+    }
+
+    Console.Write("Повторить? [Y/N] ->");
+    isContinue = (Console.ReadLine().ToUpper() == "Y");
+    Console.WriteLine();
+} while (isContinue);
