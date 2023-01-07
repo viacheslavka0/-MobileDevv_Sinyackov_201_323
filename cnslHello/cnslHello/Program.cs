d4ce04bfd50f0e78a4472a0fc03ed23dded3f199
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:40:25 +0300

    Writeline

---
+++ cnslHello/cnslHello/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Имя?");
+string? name = Console.ReadLine();
+Console.WriteLine("Город");
+string? city = Console.ReadLine();
+
+
+Console.WriteLine("Имя = " + name + ", Город = " + city);
+Console.WriteLine("Имя = {0}, Город = {1}", name, city);
+Console.WriteLine($"Имя = {name}, Город = {city}");
+
