d793aac1efe9d09e1f541fd08eee73c5acfc49d5
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:50:23 +0300

    Работа со Stack

---
+++ cnsStack/cnsStack/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Стек (Stack)
+
+// (A) Обычный стек
+
+using System.Collections;
+
+Stack x = new();
+x.Push("Рязань");
+x.Push(100500);
+
+Console.WriteLine($"Смотрим -> {x.Peek()}");
+Console.WriteLine(x.Pop());
+Console.WriteLine(x.Pop());
+
+// (Б) Обобщенный стек
