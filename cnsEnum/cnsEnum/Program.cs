42ecbce43d163ffb7357c73c7f127b39ceaff81f
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:43:06 +0300

    Enum

---
+++ cnsEnum/cnsEnum/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    //public enum SingleColor { Red, Green = 100500, Blue}
+    public enum SingleColor : byte { Red, Green = 55, Blue}
+
+    static void Main()
+    {
+        SingleColor singleColor;
+        singleColor = SingleColor.Green;
+
+
+        Console.WriteLine(singleColor);
+        Console.WriteLine(singleColor.ToString("D"));
+        Console.WriteLine(Enum.GetUnderlyingType(singleColor.GetType()));
+    }
+}
