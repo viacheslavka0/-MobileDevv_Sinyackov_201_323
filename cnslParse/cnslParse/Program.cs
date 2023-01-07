bce6c7f9f453d76377e3218468278092f41a33e3
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:47:02 +0300

    Работа с массивами

---
+++ cnslParse/cnslParse/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("number?");
+
+int value1 = int.Parse(Console.ReadLine());
+Console.WriteLine(value1);
+
+
+int value2;
+int.TryParse(Console.ReadLine(), out value2);
+Console.WriteLine(value2);
+
+int value3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(value3);
+
