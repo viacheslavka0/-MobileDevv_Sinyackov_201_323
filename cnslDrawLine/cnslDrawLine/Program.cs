2463a30b96b2fb93b93e932710b96bfd82effdf7
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:36:29 +0300

    Линия в консоли из *

---
+++ cnslDrawLine/cnslDrawLine/Program.cs
@@ -0,0 +1,19 @@
+﻿
+bool isContinue = false;
+
+do
+{
+    Console.WriteLine("Длина?");
+    //(1)
+    //int width;
+    //int.tryparse(console.readline(), out width);
+
+
+    //(2)
+    int.TryParse(Console.ReadLine(), out int width);
+
+    Console.WriteLine(new String('*', width));
+
+    Console.WriteLine("Повторить [Y/N] ?");
+    isContinue = Console.ReadLine()?.ToUpper() == "Y";
+} while (isContinue);
