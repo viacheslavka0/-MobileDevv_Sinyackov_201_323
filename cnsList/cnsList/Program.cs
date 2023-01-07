ce377a4531dae12f33da5a91dda6413fd582e01c
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:45:19 +0300

    Работа со списками

---
+++ cnsList/cnsList/Program.cs
@@ -0,0 +1,22 @@
+﻿//Список (list)
+
+// (A) Обычный список (устаревший)
+
+using System.Collections;
+
+ArrayList arrayList = new();
+arrayList.Add(123);
+arrayList.Add("Привет");
+arrayList.Add(3.14);
+//...
+
+
+// (Б) Обобщенный список
+List<string> cities = new() { "Москва", "Воронеж", "Севастополь" };
+cities.Add("Астана");
+cities.AddRange(new string[] { "Тула", "Ялта" });
+cities.RemoveAt(1);
+cities.Reverse();
+Console.WriteLine(String.Join(' ', cities));
+cities.Sort();
+Console.WriteLine(String.Join(' ', cities));
