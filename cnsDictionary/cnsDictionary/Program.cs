de3c58ef1f151c33d619fe0eb043f27631f9f24f
viacheslavka_0 <via4eslav.sinyackov@yandex.ru>
2023-01-07 20:33:27 +0300

    Работа с ключами и словами

---
+++ cnsDictionary/cnsDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Слова (Dictionary)
+//Ключ (уникальный) - значение
+
+
+Dictionary<int, string> x = new();
+x.Add(10, "Калуга");
+x.Add(40, "Псков");
+x.Add(75, "Самара");
+x.Add(15, "Брянск");
+
+
+Console.WriteLine(x[15]);
+x[40] = "Воронеж";
+x[99] = "Курск";
+
+
+foreach (var item in x)
+{
+    Console.WriteLine($"Key = {item.Key}, value = {item.Value}");
+}
+    
+    
+     
+
+
+    
+    
